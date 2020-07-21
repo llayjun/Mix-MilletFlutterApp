5605bcb5ea821ab23f295d49576caa70929f14c9
zhangyinlei <llayjun@gmail.com>
2020-07-21 09:48:08 +0800

    feat: 登录的ViewModel

--- lib/ui/login/login_vm.dart
+++ lib/ui/login/login_vm.dart
@@ -11,11 +11,11 @@ class LoginVModel with ChangeNotifier {
   /// 登录
   login(String name, String pass) {
     if (TextUtil.isEmpty(name)) {
-      Fluttertoast.showToast(msg: "请输入手机号");
+      Fluttertoast.showToast(msg: "请输入手机号手机号");
       return;
     }
     if (TextUtil.isEmpty(pass)) {
-      Fluttertoast.showToast(msg: "请输入密码");
+      Fluttertoast.showToast(msg: "请输入密码密码");
       return;
     }
     HttpManager().get(
