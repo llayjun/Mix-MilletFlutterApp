b6defb624ab65d6ae0b5967588936135def0fc40
zhangyinlei <llayjun@gmail.com>
2020-07-21 10:05:51 +0800

    feat: v1.21.2

--- lib/ui/login/login_vm.dart
+++ lib/ui/login/login_vm.dart
@@ -11,11 +11,11 @@ class LoginVModel with ChangeNotifier {
   /// 登录
   login(String name, String pass) {
     if (TextUtil.isEmpty(name)) {
-      Fluttertoast.showToast(msg: "请输入手机号手机号1211111111");
+      Fluttertoast.showToast(msg: "请输入手机号手机号1211v122222222");
       return;
     }
     if (TextUtil.isEmpty(pass)) {
-      Fluttertoast.showToast(msg: "请输入密码密码1211111111");
+      Fluttertoast.showToast(msg: "请输入密码密码1211v12222222222");
       return;
     }
     HttpManager().get(
