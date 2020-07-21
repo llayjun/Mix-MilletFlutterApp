ffa034e9891bc5231d948b5461baa5563a06fdd7
zhangyinlei <llayjun@gmail.com>
2020-07-21 10:31:20 +0800

    Merge branch 'v1.2'
    # Conflicts:
    #	lib/ui/login/login_vm.dart

--- lib/ui/login/login_vm.dart
+++ lib/ui/login/login_vm.dart
@@ -11,11 +11,11 @@ class LoginVModel with ChangeNotifier {
   /// 登录
   login(String name, String pass) {
     if (TextUtil.isEmpty(name)) {
-      Fluttertoast.showToast(msg: "请输入手机号手机号1211v122222222");
+      Fluttertoast.showToast(msg: "请输入手机号手机号");
       return;
     }
     if (TextUtil.isEmpty(pass)) {
-      Fluttertoast.showToast(msg: "请输入密码密码1211v12222222222");
+      Fluttertoast.showToast(msg: "请输入密码密码");
       return;
     }
     HttpManager().get(
